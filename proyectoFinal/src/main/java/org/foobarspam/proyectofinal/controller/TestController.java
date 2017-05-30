b4d44dfe97a87927c74be890a90a3f0b4f59f1c2
proyectofinalMVC <vicspam7@gmail.com>
2017-05-30 21:03:59 +0200

    Update TestController.java

--- proyectoFinal/src/main/java/org/foobarspam/proyectofinal/controller/TestController.java
+++ proyectoFinal/src/main/java/org/foobarspam/proyectofinal/controller/TestController.java
@@ -1,8 +1,12 @@
 package org.foobarspam.proyectofinal.controller;
 
+import org.foobarspam.proyectofinal.model.Carrera;
 import org.foobarspam.proyectofinal.service.Carrera.CarreraService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class TestController {
@@ -13,7 +17,19 @@ public class TestController {
 	public TestController(CarreraService carreraService) {
 		this.carreraService = carreraService;
 	}
-	
+
+	@RequestMapping("/carrera")
+	public String home(Model model) {
+		model.addAttribute("carrera", new Carrera());
+		return "index";
+	}
+
+	@RequestMapping( value= "carrera/guardar", method = RequestMethod.POST)
+	public String asignarCarrera(Carrera carrera) {
+		carreraService.setCarrera(carrera);
+		carreraService.guardarCarrera();
+		return "redirect:/carrera";
+	}
 	
 	
 }
